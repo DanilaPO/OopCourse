7907b7f01086ef60cabf9fde5c47090384260b64
2021 <Petr2334@yandex.ru>
2023-07-17 16:53:14 +0300

    Этап 2 Задачи Range OOP (добавление методов с интервалами)

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -1,29 +1,42 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите первое число диапазона: ");
-        double rangeBeginning = scanner.nextDouble();
+        System.out.print("Введите первое число первого интервала чисел: ");
+        double firstIntervalBeginning = scanner.nextDouble();
 
-        System.out.print("Введите конечное число диапазона: ");
-        double rangeEnd = scanner.nextDouble();
+        System.out.print("Введите конечное число первого интервала чисел: ");
+        double firstIntervalEnd = scanner.nextDouble();
 
-        System.out.print("Введите число: ");
-        double number = scanner.nextDouble();
+        System.out.print("Введите первое число второго интервала чисел: ");
+        double secondIntervalBeginning = scanner.nextDouble();
+
+        System.out.print("Введите конечное число второго интервала чисел: ");
+        double secondIntervalEnd = scanner.nextDouble();
 
         // здесь вводится диапазон
-        Range range = new Range(rangeBeginning, rangeEnd);
+        Range range = new Range(firstIntervalBeginning, firstIntervalEnd);
 
-        // это функция длины диапазона
-        System.out.println("Длина диапазона составляет: " + range.getLength());
+        // сюда вводятся точки второго диапозона для определения перечесения
+        System.out.println("Результат Пересечения интервалов: " + range.getIntersection(secondIntervalBeginning, secondIntervalEnd));
 
-        // сюда вводится точка, для определения ее принадлежности диапазону
-        if (range.isInside(number)) {
-            System.out.println("Число относится к диапазону чисел");
-        } else {
-            System.out.println("Число не относится к диапазону чисел");
+        // получение объединения двух интервалов
+        System.out.print("Результат объединения интревалов: ");
+
+        for (double[] piecesArray : range.getSetsCombining(secondIntervalBeginning, secondIntervalEnd)) {
+            System.out.print(Arrays.toString(piecesArray));
+        }
+
+        System.out.println();
+
+        // Получение разности двух интервалов
+        System.out.print("Результат разности интревалов: ");
+
+        for (double[] piecesArray : range.getSetsDifference(secondIntervalBeginning, secondIntervalEnd)) {
+            System.out.print(Arrays.toString(piecesArray));
         }
     }
 }
